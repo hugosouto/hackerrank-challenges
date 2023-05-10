6a118908e650a6b59b1e8fa15e69ee2bb58fda6d
Hugo Souto <hugomsouto@gmail.com>
2023-05-10 12:30:43 -0300

    Completition of ListComprehensions challenge

--- ListComprehensions.py
+++ ListComprehensions.py
@@ -1,18 +1,9 @@
 if __name__ == '__main__':
-    # x = int(input())
-    # y = int(input())
-    # z = int(input())
-    # n = int(input())
-
-    # Data input for testing purposes
-    x, y, z, n = 1, 2, 3, 3
-
-    a, b, c = '', '', ''
-
-    list = [[a, b, c] for a in [0, x, y, z] for b in [0, x, y, z] for c in [0, x, y, z] if sum([a, b, c]) != n]
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    list = [[a, b, c] for a in range(0, x + 1) for b in range(0, y + 1) for c in range(0, z + 1) if sum([a, b, c]) != n]
     
     print(list)
-
-    # Backlog:
-        # Eliminate duplicates
-        # Order list
