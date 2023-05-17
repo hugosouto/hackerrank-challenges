b61f22bb7d24812735afa7444c67f2ad9453b906
Hugo Souto <hugomsouto@gmail.com>
2023-05-16 22:38:37 -0300

    Advances in challenge NestedList

--- Challenges/NestedLists.py
+++ Challenges/NestedLists.py
@@ -12,16 +12,10 @@ if __name__ == '__main__':
         names.append(names0[i])
         scores.append(scores0[i])
 
-    # scores_set = sorted(list(set(scores)))
-    # second_lowest = scores_set[1]
     second_lowest = sorted(list(set(scores)))[1]
-    
-    # print('scores_set: ' + str(scores_set))
-    print('second_lowest: ' + str(second_lowest))
-    
-    # if second_lowest in scores:
-        # print(names)
 
-
-    # print(names[scores(second_lowest)])
-    # print(x for names in n names[scores(second_lowest)])
+    indices = [index for index, score in enumerate(scores) if score == second_lowest]
+    result = [names[index] for index in indices]
+    
+    for name in sorted(result):
+        print(name)
