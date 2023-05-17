9c5d6d2b1ba49ebf7668f5475603ca6acad144af
Hugo Souto <hugomsouto@gmail.com>
2023-05-16 22:42:07 -0300

    Final code

--- Challenges/NestedLists.py
+++ Challenges/NestedLists.py
@@ -1,17 +1,12 @@
 if __name__ == '__main__':
     
-    n = 5
-    names0 = ['Harry', 'Berry', 'Tina', 'Akriti', 'Harsh']
-    scores0 = [37.21, 37.21, 37.2, 41, 39]
-    
     names = []
     scores = []
-
-    for i in range(int(n)):
-           
-        names.append(names0[i])
-        scores.append(scores0[i])
-
+    
+    for x in range(int(input())):
+        names.append(input())
+        scores.append(float(input()))
+    
     second_lowest = sorted(list(set(scores)))[1]
 
     indices = [index for index, score in enumerate(scores) if score == second_lowest]
