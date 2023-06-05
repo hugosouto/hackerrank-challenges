cd9cfd30bba2e96c512079bb4309567f5e6522e7
Hugo Souto <hugomsouto@gmail.com>
2023-06-05 01:44:47 -0300

    Minor fixes

--- Challenges/FindingThePercentage.py
+++ Challenges/FindingThePercentage.py
@@ -1,19 +1,19 @@
-# Artificial data to simulate HackerRank input
-input = ('''3
-Krishna 67 68 69
-Arjun 70 98 63
-Malika 52 56 60
-Malika''')
-
-# Import package to simulate HackerHack input
-from io import StringIO
-
-# Convert the string data to a file-like object
-data_io = StringIO(input)
-
-# Modified HackerHack code stub
 if __name__ == '__main__':
-    
+
+    # Artificial data to simulate HackerRank input
+    input = ('''3
+    Krishna 67 68 69
+    Arjun 70 98 63
+    Malika 52 56 60
+    Malika''')
+
+    # Import package to simulate HackerHack input
+    from io import StringIO
+
+    # Convert the string data to a file-like object
+    data_io = StringIO(input)
+
+    # Modified HackerHack code stub
     n = int(data_io.readline())
     student_marks = {}
 
@@ -23,6 +23,6 @@ if __name__ == '__main__':
         student_marks[name] = scores
     query_name = data_io.readline().strip()
     
-# Solution
+    # Solution
     average_score = sum(student_marks[query_name])/len(student_marks[query_name])
     print("{:.2f}".format(average_score))
