43f7d5f17d014c3399726a228d5762015fad3a27
Hugo Souto <hugomsouto@gmail.com>
2023-06-06 13:14:04 -0300

    Create file

---
+++ Artificial Intelligence/BotSavesPrincess.py
@@ -0,0 +1,36 @@
+# Initial code
+# 
+# #!/usr/bin/python
+# 
+# def displayPathtoPrincess(n,grid):
+# #print all the moves here
+# 
+# m = int(input())
+# grid = [] 
+# for i in range(0, m): 
+#     grid.append(input().strip())
+# 
+# displayPathtoPrincess(m,grid)
+
+# Artificial data to simulate HackerRank input
+input = ('''3
+---
+-m-
+p--''')
+
+# Import package to simulate HackerHack input
+from io import StringIO
+
+# Convert the string data to a file-like object
+data_io = StringIO(input)
+
+# Modified HackerHack code stub
+def displayPathtoPrincess(n,grid):
+    print(0)#print all the moves here
+
+m = int(data_io.readline())
+grid = [] 
+for i in range(0, m): 
+    grid.append(input().strip())
+
+displayPathtoPrincess(m,grid)
