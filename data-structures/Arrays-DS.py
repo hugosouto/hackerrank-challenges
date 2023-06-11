173765762a42173f52cbab2e1860dbacde2f1aaf
Hugo Souto <hugomsouto@gmail.com>
2023-06-11 00:34:22 -0300

    Challenge solved

--- data-structures/Arrays-DS.py
+++ data-structures/Arrays-DS.py
@@ -1,31 +1,45 @@
-#!/bin/python3
-
+# Imports
 import math
 import os
 import random
 import re
 import sys
 
-#
-# Complete the 'reverseArray' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts INTEGER_ARRAY a as parameter.
-#
+import tempfile
+from io import StringIO
 
+
+# Artificial data
+input = ('''4
+1 4 3 2''')
+
+print(input)
+
+
+# Convert the string data to a file-like object
+data_io = StringIO(input)
+
+
+# Solution
 def reverseArray(a):
-    # Write your code here
+    return a[::-1]
+    # Alternative solution
+    # a.reverse()
+    # return a
+    
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    # Execute when the module is not initialized from an import statement.
 
-    arr_count = int(input().strip())
+    # Create a temporary file
+    fptr = tempfile.NamedTemporaryFile(mode='w', delete=True)
 
-    arr = list(map(int, input().rstrip().split()))
-
+    arr_count = int(data_io.readline().strip())
+    arr = list(map(int, data_io.readline().split()))
     res = reverseArray(arr)
 
     fptr.write(' '.join(map(str, res)))
     fptr.write('\n')
-
     fptr.close()
+
+    print(res)
