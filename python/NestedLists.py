74269de9bcca8d507d6051ef392f10cc7a73367c
Hugo Souto <hugomsouto@gmail.com>
2023-06-15 22:31:37 -0300

    Minor refactoring

--- python/NestedLists.py
+++ python/NestedLists.py
@@ -3,19 +3,19 @@
 # It uses a for loop to print each value in separated line.
 
 if __name__ == '__main__':
-    
+
     names = []
     scores = []
-    
-    for x in range(int(input())):
+
+    for _ in range(int(input())):
         names.append(input())
         scores.append(float(input()))
-    
+
     second_lowest = sorted(list(set(scores)))[1]
 
     indices = [index for index, score in enumerate(scores) if score == second_lowest]
     result = [names[index] for index in indices]
-    
+
     for name in sorted(result):
         print(name)
 
@@ -36,18 +36,18 @@ if __name__ == '__main__':
     n = 5
     names0 = ['Harry', 'Berry', 'Tina', 'Akriti', 'Harsh']
     scores0 = [37.21, 37.21, 37.2, 41, 39]
-    
+
     names = []
     scores = []
 
-    for i in range(int(n)):
-           
+    for i in range(n):
+
         names.append(names0[i])
         scores.append(scores0[i])
-    
+
     second_lowest = sorted(list(set(scores)))[1]
 
     indices = [index for index, score in enumerate(scores) if score == second_lowest]
     result = [names[index] for index in indices]
-    
+
     print(*sorted(list(result)), sep="\n")
