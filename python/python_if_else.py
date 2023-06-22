e3c7fa770105917fb347bd54f68660bc9c6bff35
Hugo Souto <hugomsouto@gmail.com>
2023-06-22 14:13:13 -0300

    Challenge solved

--- python/python_if_else.py
+++ python/python_if_else.py
@@ -6,62 +6,26 @@ import random
 import re
 import sys
 
-from io import StringIO  # Import package to simulate HackerHack input
-
-# Artificial data to simulate HackerRank sample input
-
-
-class RandomNumbers:
-    def random_numbers():
-
-        n = random.randint(1, 20)
-        print(f'Number of samples: {n}')
-
-        list = []
-        for _ in range(n):
-            number = random.randint(1, 101)
-            # Print without new line
-            print('number', number)
-
-        return
-
-
-random_numbers()
-
-
-# sample_input = str(random_numbers())
-# print(f'The sample input is:\n{random_numbers()}\n\nThe output is:')
-
-# """
-
-# Convert the string data to a file-like object
-data_io = StringIO(sample_input)
-
-# Number of test cases
-n_tests = int(data_io.readline().strip())
-
-# Conditions
+# Given an integer, n, perform the following conditional actions:
 # 1 < n < 100
 # If n is odd, print Weird
 # If n is even and in the inclusive range of 2 to 5, print Not Weird
 # If n is even and in the inclusive range of 6 to 20, print Weird
 # If n is even and greater than 20, print Not Weird
 
-# Solution
+# Solution:
 if __name__ == '__main__':
 
-    @RandomNumbers.random_numbers
-    def evaluate_numbers():
-        for _ in range(n_tests):
+    # Artificial data to simulate HackerRank sample input
+    n = random.randint(1, 20)
 
-            n = int(data_io.readline().strip())
-            print(f'{n} is', end=' ')
+    print(f'{n} is', end=' ')
 
-            if n % 2 == 1:
-                print('Weird')
-            elif n >= 2 and n <= 5:
-                print('Not Weird')
-            elif n >= 6 and n <= 20:
-                print('Weird')
-            elif n > 20:
-                print('Not Weird')
+    if n % 2 == 1:
+        print('Weird')
+    elif n >= 2 and n <= 5:
+        print('Not Weird')
+    elif n >= 6 and n <= 20:
+        print('Weird')
+    elif n > 20:
+        print('Not Weird')
