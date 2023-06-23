a9429887bc0e996021d1b51578fd66a353203886
Hugo Souto <hugomsouto@gmail.com>
2023-06-23 20:04:21 -0300

    Challenge solved

--- python/tuples.py
+++ python/tuples.py
@@ -1,8 +1,29 @@
+'''
+Task:
+
+• Given an integer, n, and n space-separated integers as input, create a tuple, t, of those n 
+    integers. Then compute and print the result of hash(t).
+
+'''
+
+# Deveopment:
+
+# Imports the required libraries
 from io import StringIO
 
-input = (f'''2
+## Creates raw artificial data
+raw = (f'''2
 1 2''')
 
-t = (1, 2)
+## Simulates a user input
+input = StringIO(raw)
 
-print(hash(t))
+# Solution:
+if __name__ == '__main__':
+
+    n = int(input.readline())
+    integer_list = map(int, input.readline().split())
+    
+    integer_tuple = tuple(integer_list)
+
+    print(hash(integer_tuple))
