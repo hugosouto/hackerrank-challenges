994bcc1d8114e436bb9826154b976b8216539254
Hugo Souto <hugomsouto@gmail.com>
2023-06-24 06:59:29 -0300

    Create challenge

---
+++ python/swap_case.py
@@ -0,0 +1,33 @@
+# About the Challenge:
+
+# You are given a string and your task is to swap cases. In other words, convert all lowercase 
+# letters to uppercase letters and vice versa.
+# 
+# For Example:
+# • Www.HackerRank.com → wWW.hACKERrANK.COM
+# • Pythonist 2 → pYTHONIST 2  
+# 
+# Function Description:
+# Complete the swap_case function in the editor below.
+# swap_case has the following parameters:
+# • string s: the string to modify
+# 
+# Returns:
+# • string: the modified string
+# 
+# Input Format:
+# A single line containing a string .
+# 
+# Constraints:
+# 0 < len(s) <= 1000
+
+input = "HackerRank.com presents 'Pythonist 2'."
+
+def swap_case(s):
+    return s.swapcase(s)
+
+if __name__ == '__main__':
+    s = input
+    result = swap_case(s)
+    print(result)
+
