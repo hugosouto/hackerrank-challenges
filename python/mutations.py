fc80f0d299f61edc55c3fbc75b4b9975d6b99e61
Hugo Souto <hugomsouto@gmail.com>
2023-06-27 22:28:10 -0300

    Challenge solved

--- python/mutations.py
+++ python/mutations.py
@@ -1,8 +1,15 @@
+from io import StringIO
+
+raw_input = ('''abracadabra
+5 k''')
+
+input = StringIO(raw_input)
+
 def mutate_string(string, position, character):
-    return
+    return string[0:position] + character + string[position + 1:]
 
 if __name__ == '__main__':
-    s = input()
-    i, c = input().split()
+    s = input.readline()
+    i, c = input.readline().split()
     s_new = mutate_string(s, int(i), c)
     print(s_new)
