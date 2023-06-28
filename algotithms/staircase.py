2603224ca117ac2cdea971753468f0cd65ef887f
Hugo Souto <hugomsouto@gmail.com>
2023-06-27 21:55:21 -0300

    Creates challenge

---
+++ algotithms/staircase.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import random
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+input = random.randint(1, 101)
+
+def staircase(n):
+    # Write your code here
+    return
+
+if __name__ == '__main__':
+    n = int(input)
+
+    staircase(n)
