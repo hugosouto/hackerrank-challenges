c00f08721558d9cabefc6ad04fe881746c3b03d5
Hugo Souto <hugomsouto@gmail.com>
2023-06-30 00:38:01 -0300

    Challenge solved

--- python/find_a_string.py
+++ python/find_a_string.py
@@ -5,10 +5,19 @@ CDC''')
 
 input = StringIO(raw_input)
 
-def count_substring(string, sub_string):
-    return
+def count_substring(string, sub_string):    
+    
+    indexes = []
+
+    for i in range(0, len(string)):
+        indexes.append(string.find(sub_string, i, i+len(sub_string)))
+
+    valid_indexes = [i for i in indexes if i != -1]
+
+    return len(valid_indexes)
 
 if __name__ == '__main__':
+    
     string = input.readline().strip()
     sub_string = input.readline().strip()
     
