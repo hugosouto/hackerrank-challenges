9bc1f9c6d9b89cffaf8d8a3ea6f15169fdc2953d
Hugo Souto <hugomsouto@gmail.com>
2023-06-30 00:41:13 -0300

    Documents file

--- python/find_a_string.py
+++ python/find_a_string.py
@@ -1,9 +1,15 @@
+# Import to simulate HackerRank's input
 from io import StringIO
 
+# Input sample to challenge
 raw_input = ('''ABCDCDC
 CDC''')
 
+# Convert input to object to simulate HackerRank's input
 input = StringIO(raw_input)
+
+# Solution:
+    # TODO: Refactor this function.
 
 def count_substring(string, sub_string):    
     
