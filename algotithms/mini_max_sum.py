f3cac92251d5fec98107c466846cbd9ea422e65b
Hugo Souto <hugomsouto@gmail.com>
2023-07-02 23:30:49 -0300

    Initiate challenge

---
+++ algotithms/mini_max_sum.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from io import StringIO
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+raw_input = "1 2 3 4 5"
+
+input = StringIO(raw_input)
+
+def miniMaxSum(arr):
+    
+    return
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input.readline().rstrip().split()))
+
+    miniMaxSum(arr)
