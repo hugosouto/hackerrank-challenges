8ba410af931638f2a5cb9ef2cedb1fcc82473097
Hugo Souto <hugomsouto@gmail.com>
2023-07-03 00:14:35 -0300

    Challenge solved

--- python/string_validators.py
+++ python/string_validators.py
@@ -6,3 +6,34 @@ input = StringIO(raw_data)
 
 if __name__ == '__main__':
     s = input.readline()
+
+    v = 'False'
+    for _ in s:
+        if str.isalnum(s) == True: 
+            v = 'True'
+    print(v)
+    
+    v = 'False'
+    for _ in s:
+        if str.isalpha(s) == True:
+            v = 'True'
+    print(v)
+    
+    v = 'False'
+    for _ in s:
+        if str.isdigit(s) == True:
+            v = 'True'
+    print(v)
+    
+    v = 'False'
+    for _ in s:
+        if str.islower(s) == True:
+            v = 'True'
+    print(v)
+    
+    v = 'False'
+    for _ in s:
+        if str.isupper(s) == True:
+            v = 'True'
+    print(v)
+    
