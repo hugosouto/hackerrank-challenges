33578a87cb0d86a4d6bd2715d63daf8f50f28ebe
Hugo Souto <hugomsouto@gmail.com>
2023-07-08 00:36:06 -0300

    Improve code for random input

--- python/text_wrap.py
+++ python/text_wrap.py
@@ -1,10 +1,20 @@
 # Imports
+import random
+import string
 import textwrap
 from io import StringIO
 
-# Data
-raw_data = """ABCDEFGHIJKLIMNOQRSTUVWXYZ
-4"""
+# Function for Input Generation
+def get_random_string(length):
+    letters = string.ascii_uppercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+str_lenth = random.randint(1, 100)
+result_str = get_random_string(str_lenth)
+max_width = random.randint(1, str_lenth)  # Make sure max_width is not larger than the string length
+
+# Input Simulation
+raw_data = (f'{result_str}\n{max_width}')
 
 input = StringIO(raw_data)
 
