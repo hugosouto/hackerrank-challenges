715a5510ac65422646a7ffcccdb074137751e33d
Hugo Souto <hugomsouto@gmail.com>
2023-07-09 20:43:36 -0300

    Create challenge

---
+++ python/designer_door_mat.py
@@ -0,0 +1,64 @@
+'''
+# Problem
+
+Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
+
+Mat size must be NxM. (N is an odd natural number, and M is 3 times .)
+The design should have 'WELCOME' written in the center.
+The design pattern should only use |, . and - characters.
+
+# Sample Designs
+
+    Size: 7 x 21 
+    ---------.|.---------
+    ------.|..|..|.------
+    ---.|..|..|..|..|.---
+    -------WELCOME-------
+    ---.|..|..|..|..|.---
+    ------.|..|..|.------
+    ---------.|.---------
+    
+    Size: 11 x 33
+    ---------------.|.---------------
+    ------------.|..|..|.------------
+    ---------.|..|..|..|..|.---------
+    ------.|..|..|..|..|..|..|.------
+    ---.|..|..|..|..|..|..|..|..|.---
+    -------------WELCOME-------------
+    ---.|..|..|..|..|..|..|..|..|.---
+    ------.|..|..|..|..|..|..|.------
+    ---------.|..|..|..|..|.---------
+    ------------.|..|..|.------------
+    ---------------.|.---------------
+
+# Input Format
+
+A single line containing the space separated values of N and M.
+
+# Constraints
+
+5 < N < 101
+15 < M < 303
+
+# Output Format
+
+Output the design pattern.
+
+# Sample Input
+
+9 27
+
+# Sample Output
+
+------------.|.------------
+---------.|..|..|.---------
+------.|..|..|..|..|.------
+---.|..|..|..|..|..|..|.---
+----------WELCOME----------
+---.|..|..|..|..|..|..|.---
+------.|..|..|..|..|.------
+---------.|..|..|.---------
+------------.|.------------
+'''
+
+# Read input from STDIN. Print output to STDOUT
