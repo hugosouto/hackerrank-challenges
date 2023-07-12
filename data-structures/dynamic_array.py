f56bda03f232ace728f9215c9aa9659e714d2273
Hugo Souto <hugomsouto@gmail.com>
2023-07-11 23:22:12 -0300

    Minor advances

--- data-structures/dynamic_array.py
+++ data-structures/dynamic_array.py
@@ -8,17 +8,6 @@ import sys
 import tempfile
 from io import StringIO
 
-# Sample Input
-
-raw_data = """2 5
-1 0 5
-1 1 7
-1 0 3
-2 1 0
-2 1 1"""
-
-input = StringIO(raw_data)
-
 #
 # Complete the 'dynamicArray' function below.
 #
@@ -28,10 +17,24 @@ input = StringIO(raw_data)
 #  2. 2D_INTEGER_ARRAY queries
 #
 
-def dynamicArray(n, queries):
-    # Write your code here
-    return n, queries
+# Sample Input
+raw_data = """5 5
+1 0 5
+1 1 7
+1 0 3
+2 1 0
+2 1 1"""
 
+input = StringIO(raw_data)
+
+# Solution
+def dynamicArray(n, queries):
+    arr = []
+    for i in range(n):
+        arr.append(i)
+    return arr
+
+# Main
 if __name__ == '__main__':
     # Create a temporary file
     fptr = tempfile.NamedTemporaryFile(mode='w', delete=True) 
