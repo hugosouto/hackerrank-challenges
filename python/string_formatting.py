bb430c55da3170c492c128eb05e931b197a8ace0
Hugo Souto <hugomsouto@gmail.com>
2023-07-12 23:27:38 -0300

    Challenge solved

--- python/string_formatting.py
+++ python/string_formatting.py
@@ -1,7 +1,15 @@
+import random
+
+input = random.randint(1, 20)
+
 def print_formatted(number):
-    # your code goes here
-    return
+    padding = len(str(bin(number)[2:]))
+    for i in range(1, number + 1):
+        print(str(int(i)).rjust(padding),  
+              str(oct(i))[2:].rjust(padding),
+              str(hex(i)).upper()[2:].rjust(padding),
+              str(bin(i))[2:].rjust(padding))
 
 if __name__ == '__main__':
-    n = int(input())
+    n = int(input)
     print_formatted(n)
