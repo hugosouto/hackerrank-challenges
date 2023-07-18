623ab1a825fa64d143ee87159f56f4b66ddc28a2
Hugo Souto <hugomsouto@gmail.com>
2023-07-17 23:49:22 -0300

    Create challenge

---
+++ python/alphabet_rangoli.py
@@ -0,0 +1,18 @@
+# Imports
+import random
+from io import StringIO
+
+# Data
+size = random.randint(1, 26)
+
+input = StringIO(str(size))
+
+# Solution
+def print_rangoli(size):
+    # your code goes here
+    return
+
+# Main
+if __name__ == '__main__':
+    n = int(input.readline())
+    print_rangoli(n)
