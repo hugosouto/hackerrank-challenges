76f30e1c3ff9d55a3d11e00fe640d99200c15794
Hugo Souto <hugomsouto@gmail.com>
2023-07-19 19:12:16 -0300

    Advances using "-" as index[0] on letters

--- python/alphabet_rangoli.py
+++ python/alphabet_rangoli.py
@@ -1,15 +1,40 @@
 # Imports
 import random
+import string
 from io import StringIO
 
 # Data
-size = random.randint(1, 26)
+# size = random.randint(1, 26)
+size = 5
 
 input = StringIO(str(size))
 
 # Solution
 def print_rangoli(size):
-    # your code goes here
+    
+    dash = "-"
+    # letters = string.ascii_lowercase
+    letters = [dash]
+    letters.extend(string.ascii_lowercase)
+    # print(letters)
+    
+    for i in range(size-1):
+        print(dash.rjust(size//2) + 
+            dash*(size//2) +
+            letters[size-i] +
+            dash*(size//2) +
+            str(dash.ljust(size//2))
+        )
+    
+    print(dash.join(reversed(letters[1:size+1])))
+
+    for i in reversed(range(size-1)):
+        print(dash.rjust(size//2) + 
+            dash*(size//2) +
+            letters[size-i] +
+            dash*(size//2) +
+            str(dash.ljust(size//2))
+        )
     return
 
 # Main
