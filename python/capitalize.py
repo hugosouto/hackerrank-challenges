900f7c90de1ef569091abc1c8aeaeada20fc7417
Hugo Souto <hugomsouto@gmail.com>
2023-07-26 01:00:48 -0300

    Solved challenge

--- python/capitalize.py
+++ python/capitalize.py
@@ -7,18 +7,35 @@ import random
 import re
 import sys
 import tempfile
+import string
 from io import StringIO
 
 # Sample Input
 # raw_data = "chris alan"
-raw_data = "1 w 2 r 3g"
+# raw_data = "1 w 2 r  3g"
+raw_data = "132 456 Wq  m e"
 
 # Data
 input = StringIO(raw_data)
 
 # Solution
 def solve(s):
-    return str.title(s)
+
+    list = []
+    for i in s.split(' '):
+        
+        if len(i) == 0:
+            i = ''
+        elif i[0] in string.digits:
+            pass
+        else:
+            i = i.title()
+
+        list.append(i)
+
+    final_string = str(' '.join(list))
+
+    return final_string
 
 # Main
 if __name__ == '__main__':
