bc543231305258305ac46894decea2ea3bb48fe8
Hugo Souto <hugomsouto@gmail.com>
2023-07-26 23:42:32 -0300

    Challenge solved

--- algotithms/grading_students.py
+++ algotithms/grading_students.py
@@ -15,8 +15,25 @@ raw_data = '''4
 input = StringIO(raw_data)
 
 # Solution
+
 def gradingStudents(grades):
-    return ''
+    """
+    This function receives a list of grades and returns a new list with the rounded grades according to the following rules:
+    - If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+    - If the value of grade is less than 38, no rounding occurs.
+    :param grades: list of integers representing the grades.
+    :return: list of integers representing the rounded grades.
+    """
+    new_grades = []
+
+    for i in grades:
+        if i >= 38 and i % 5 >= 3:
+            i = i + 5-(i % 5)
+        else:
+            i = i
+        new_grades.append(i)
+
+    return new_grades
 
 # Main
 if __name__ == '__main__':
@@ -31,7 +48,8 @@ if __name__ == '__main__':
         grades.append(grades_item)
 
     result = gradingStudents(grades)
-
+    print(result)
+    
     fptr.write('\n'.join(map(str, result)))
     fptr.write('\n')
 
