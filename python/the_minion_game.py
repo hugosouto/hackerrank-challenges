6f67c0b1d942d5a57bcf125be9d82b97a57b1765
Hugo Souto <hugomsouto@gmail.com>
2023-08-12 12:47:00 -0300

    Initiate challenge

---
+++ python/the_minion_game.py
@@ -0,0 +1,23 @@
+'''
+Challenge: The Minion Game
+Difficulty: Medium
+Topic: Python (Basic)
+Max Score: 40
+Success Rate: 86.75%
+Task: Given a string, judge the winner of the minion game.
+Problem: https://www.hackerrank.com/challenges/the-minion-game/problem
+'''
+
+from io import StringIO
+
+raw_data = "BANANA"
+
+input = StringIO(raw_data)
+
+def minion_game(string):
+    # your code goes here
+    return
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
