dbade8ec4227d572503afcc32010f88dc2779016
Hugo Souto <hugomsouto@gmail.com>
2023-08-12 13:11:10 -0300

    Advances in solution

--- python/the_minion_game.py
+++ python/the_minion_game.py
@@ -8,16 +8,35 @@ Task: Given a string, judge the winner of the minion game.
 Problem: https://www.hackerrank.com/challenges/the-minion-game/problem
 '''
 
+# Imports
+import string
 from io import StringIO
 
+# Data
 raw_data = "BANANA"
 
+# Simulate User Input
 input = StringIO(raw_data)
 
+# Solution
 def minion_game(string):
-    # your code goes here
+    
+    # s, k = []
+    for i in string:
+
+        # print(i)
+
+        # TODO: Make the condition: 'if i is not equal to vowels:'.
+        if str(i).upper == ['A', 'E', 'I', 'O', 'U']:
+            # TODO: Multiply each letter for each letter.
+            print(i*i)
+            # s.append(a)
+        # elif str(i).upper != ['A', 'E', 'I', 'O', 'U']:
+            # print(i*i)
+
     return
 
+# Main
 if __name__ == '__main__':
-    s = input()
+    s = input.readline()
     minion_game(s)
