a861e6cfcc58ed8103212153bbeb52508fefac7b
Hugo Souto <hugomsouto@gmail.com>
2023-09-22 18:50:08 -0300

    Initiate challenge

---
+++ python/itertools_product.py
@@ -0,0 +1,9 @@
+'''
+Challenge: itertools.product()
+Difficulty: Easy
+Topic: Python (Basic)
+Max Score: 10
+Success Rate: 98.16%
+Task: Find the cartesian product of 2 sets.
+Problem: https://www.hackerrank.com/challenges/itertools-product/problem
+'''
