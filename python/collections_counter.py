1cb652a0db665c1af9e898fcf31f2ad5f74968e2
Hugo Souto <hugomsouto@gmail.com>
2023-09-24 11:33:59 -0300

    Initiate challenge

---
+++ python/collections_counter.py
@@ -0,0 +1,35 @@
+'''
+Challenge: collections.Counter()
+Difficulty: Easy
+Topic: Problem Solving (Basic)
+Max Score: 10
+Success Rate: 97.44%
+Task: Use a counter to sum the amount of money earned by the shoe shop owner.
+Problem: https://www.hackerrank.com/challenges/collections-counter/problem
+'''
+
+# Imports
+from io import StringIO
+
+# Simulate input from HackerRank
+raw_data = '''10
+2 3 4 5 6 8 7 6 5 18
+6
+6 55
+6 45
+6 55
+4 40
+18 60
+10 50
+'''
+# Expected Output
+# 200
+
+# Data
+input = StringIO(raw_data)
+
+# Solution
+
+# Main
+# if __name__ == '__main__':
+    # data = input.readline().strip()
