48eefc643d7b06116c268753a1d6551febb5cc49
Hugo Souto <hugomsouto@gmail.com>
2023-09-26 08:13:46 -0300

    Initiate challenge

---
+++ python/polar_coordinates.py
@@ -0,0 +1,31 @@
+'''
+Challenge: Polar Coordinates
+Difficulty: Easy
+Topic: Python (Basic)
+Max Score: 10
+Success Rate: 96.42%
+Task: Convert complex numbers to polar coordinates.
+Problem: https://www.hackerrank.com/challenges/polar-coordinates/problem
+'''
+
+# Imports
+from io import StringIO
+
+# Simulate input from HackerRank
+raw_data = '''  1+2j'''
+    # Expected Output:
+    #  2.23606797749979 
+    #  1.1071487177940904
+    # 
+    # Note: The output should be correct up to 3 decimal places.
+
+# Data
+input = StringIO(raw_data)
+
+# Function
+def function(args):
+    return
+
+# Main
+if __name__ == '__main__':
+    input = input.readline()
