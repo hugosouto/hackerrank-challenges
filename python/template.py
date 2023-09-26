cf0f50452f5c1e993f5d52ee021eea8d22d5dee9
Hugo Souto <hugomsouto@gmail.com>
2023-09-26 08:11:21 -0300

    Add file

---
+++ python/template.py
@@ -0,0 +1,29 @@
+'''
+Challenge: 
+Difficulty: 
+Topic: 
+Max Score: 
+Success Rate: 
+Task: 
+Problem: https://www.hackerrank.com/challenges/[...]/problem
+'''
+
+# Imports
+from io import StringIO
+
+# Simulate input from HackerRank
+raw_data = '''
+'''
+    # Expected Output:
+    # 
+
+# Data
+input = StringIO(raw_data)
+
+# Function
+def function(args):
+    return
+
+# Main
+if __name__ == '__main__':
+    input = input.readline()
