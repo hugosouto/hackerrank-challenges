4c8ab7ac3e60bb79d531177fe3524ab4218a1d59
Hugo Souto <hugomsouto@gmail.com>
2023-09-29 08:24:09 -0300

    Partialy solved

--- python/polar_coordinates.py
+++ python/polar_coordinates.py
@@ -10,22 +10,32 @@ Problem: https://www.hackerrank.com/challenges/polar-coordinates/problem
 
 # Imports
 from io import StringIO
+from cmath import phase
 
 # Simulate input from HackerRank
-raw_data = '''  1+2j'''
+raw_data = '  1+2j'
     # Expected Output:
     #  2.23606797749979 
     #  1.1071487177940904
-    # 
-    # Note: The output should be correct up to 3 decimal places.
+
+raw_data = '-1-5j'
+    # Expected Output
+    # 5.09901951359
+    # -1.76819188664
 
 # Data
 input = StringIO(raw_data)
 
 # Function
-def function(args):
-    return
+def polar_coordinates(x, y):
+    r = phase(complex(x, y))
+    phi = abs(complex(x, y))
+    return print(round(r, 3), round(phi, 3), sep='\n')
 
 # Main
 if __name__ == '__main__':
-    input = input.readline()
+    x, y = input.readline().split('1')
+    y, j = y.split('')
+    x, y = int(x), int(y)
+
+    polar_coordinates(x, y)
