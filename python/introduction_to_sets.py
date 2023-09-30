669bf795cbd31966da908205beadda9950cb6e56
Hugo Souto <hugomsouto@gmail.com>
2023-09-30 08:05:41 -0300

    Initiate challenge

---
+++ python/introduction_to_sets.py
@@ -0,0 +1,30 @@
+'''
+Challenge: Introduction to Sets
+Difficulty: Easy
+Topic: Python (Basic)
+Max Score: 10
+Success Rate: 98.53% 
+Task: Use the set tool to compute the average.
+Problem: https://www.hackerrank.com/challenges/py-introduction-to-sets/problem
+'''
+
+# Imports
+from io import StringIO
+
+# Simulate input from HackerRank
+raw_data = '''10
+161 182 161 154 176 170 167 171 170 174
+'''
+    # Expected Output:
+    # 169.375
+
+# Data
+input = StringIO(raw_data)
+
+# Function
+def function(args):
+    return
+
+# Main
+if __name__ == '__main__':
+    input = input.readline()
