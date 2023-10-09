deee1b2f833036e49a1285e7bca4ab7314ae60dc
Hugo Souto <hugomsouto@gmail.com>
2023-10-09 08:59:26 -0300

    Add refactor with ChatGPT aid

--- algotithms/implementation/between_two_sets.py
+++ algotithms/implementation/between_two_sets.py
@@ -38,21 +38,33 @@ input = StringIO(raw_data)
 
 # Function
 def getTotalX(a, b):
-    
+    '''
+    Given two lists of integers, a and b, find the number of integers 
+    that satisfy certain criteria relative to the two sets.
+    '''
+
     max_a = max(a)
     min_b = min(b)
-    
-    rng = range(max_a, min_b + 1)
-    condition_1, condition_2 = [], []
-    for number in rng:
-        if all(number % item == 0 for item in a):
-            condition_1.append(number)
-    
-    for number in condition_1:
-        if all(item % number == 0 for item in b):
-            condition_2.append(number)
-    
-    total = len(condition_2)
+
+    # Original code
+    # rng = range(max_a, min_b + 1)
+    # condition_1, condition_2 = [], []
+    # for number in rng:
+    #     if all(number % item == 0 for item in a):
+    #         condition_1.append(number)
+    # 
+    # for number in condition_1:
+    #     if all(item % number == 0 for item in b):
+    #         condition_2.append(number)
+    # 
+    # total = len(condition_2)
+
+    # ChatGPT Assisted Refactoring
+    numbers_meeting_criteria = [number for number in range(max_a, min_b + 1) 
+                                if all(number % item == 0 for item in a) 
+                                and all(item % number == 0 for item in b)]
+
+    total = len(set(numbers_meeting_criteria))
 
     return total
 
