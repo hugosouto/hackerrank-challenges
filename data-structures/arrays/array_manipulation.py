f6a92ab6d87503feaf4be8bafcc25de72a140cbe
Hugo Souto <hugomsouto@gmail.com>
2023-10-17 14:01:51 -0300

    Solve challenge with prefix sum (ChatGPT assisted)

--- data-structures/arrays/array_manipulation.py
+++ data-structures/arrays/array_manipulation.py
@@ -21,21 +21,30 @@ import time
 from io import StringIO
 
 # Simulate input from HackerRank
-raw_data = '''5 3
-1 2 100
-2 5 100
-3 4 100
-'''
+# raw_data = '''5 3
+# 1 2 100
+# 2 5 100
+# 3 4 100
+# '''
     # Expected Output:
     # 200
 
 # Import txt files as string
-raw_data = open('C:\\Users\\Hugo\\GitHub\\hackerrank-challenges\\data-structures\\arrays\\array_manipulation_test_case_3.txt', 'r').read()
+# raw_data = open('C:\\Users\\Hugo\\GitHub\\hackerrank-challenges\\data-structures\\arrays\\array_manipulation_test_case_3.txt', 'r').read()
     # Expected Output:
     # 6314
+
 raw_data = open('C:\\Users\\Hugo\\GitHub\\hackerrank-challenges\\data-structures\\arrays\\array_manipulation_test_case_6.txt', 'r').read()
     # Expected Output:
     # 7515267971
+
+# raw_data = '''4 3
+# 2 3 603
+# 1 1 286
+# 4 4 882
+# '''
+    # Expected Output:
+    # 882
 
 # Data
 input = StringIO(raw_data)
@@ -61,6 +70,7 @@ def arrayManipulation(n, queries):
     #     for i in range(q[0]-1, q[1]):
     #         arr[i] += q[2]
     #         # print(arr)
+    # return max(arr)
 
     # Version 2
     # arr = [0] * n
@@ -68,13 +78,15 @@ def arrayManipulation(n, queries):
     #     slice = arr[queries[q][0]-1:queries[q][1]]
     #     add = queries[q][2]
     #     arr[queries[q][0]-1:queries[q][1]] = [i + add for i in slice]
-    
+    # return max(arr)
+
     # Version 3
     # arr = [0] * n
     # for q in range(len(queries)):
         # slice = arr[queries[q][0]-1:queries[q][1]]
         # add = queries[q][2]
         # arr[queries[q][0]-1:queries[q][1]] = map(lambda x: x + add, slice)
+    # return max(arr)
 
     # Version Extra (with NumPy)
     # import numpy as np
@@ -86,34 +98,53 @@ def arrayManipulation(n, queries):
     #     number_to_add = queries[q][2]
     #     # print(np.array(arr[queries[q][0]-1:queries[q][1]]).sum())
     #     arr[queries[q][0]-1:queries[q][1]] = list(map(lambda x: x + number_to_add, slice))
-    
+    # return max(arr)
+
     # Version 4 (with Recursive Function)
     # arr = [0] * n
     # query = 0
     # remaining = len(queries) - query
     # recAddQuery(remaining, query, arr)
+    # return max(arr)
 
     # Version 5 (ChatGPT-based)
     # arr = [0] * n
     # for query in queries:
     #     a, b, k = query
     #     arr[a-1:b] = [i + k for i in arr[a-1:b]]
-    
+    # return max(arr)
+
     # Version 6 (ChatGPT-based)
     # arr = [0] * n
     # for query in queries:
     #     a, b, k = query
     #     for i in range(a-1, b):
     #         arr[i] += k
+    # return max(arr)
 
     # Version 7 (ChatGPT-based)
+    # arr = [0] * n
+    # for query in queries:
+    #     a, b, k = query
+    #     for x in range(b-(a-1)):
+    #         arr[(a-1) + x] += k
+    # return max(arr)
+
+    # Version 8 (with Prefix Sum, ChatGPT-assisted)
     arr = [0] * n
     for query in queries:
         a, b, k = query
-        for x in range(b-(a-1)):
-            arr[(a-1) + x] += k
-
-    return max(arr)
+        arr[a - 1] += k
+        if b < n:
+            arr[b] -= k
+    
+    max_value = 0
+    current_sum = 0
+    for val in arr[:]:
+        current_sum += val
+        max_value = max(max_value, current_sum)
+    
+    return max_value
 
 # Main
 if __name__ == '__main__':
