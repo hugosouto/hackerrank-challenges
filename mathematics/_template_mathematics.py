63d9935efb73f7ab86e3757c2737dcf634f862ba
Hugo Souto <hugomsouto@gmail.com>
2023-10-21 15:53:50 -0300

    Add file

---
+++ mathematics/_template_mathematics.py
@@ -0,0 +1,45 @@
+'''
+Challenge: 
+Difficulty: 
+Topic: 
+Max Score: 
+Success Rate: 
+Task: 
+Problem: https://www.hackerrank.com/challenges/[...]/problem
+'''
+
+# Imports
+import math
+import os
+import random
+import re
+import sys
+import tempfile
+from io import StringIO
+
+# Simulate input from HackerRank
+raw_data = '''
+'''
+    # Expected Output:
+    # 
+
+# Data
+input = StringIO(raw_data)
+
+# Function
+def function(args):
+    return result
+
+# Main
+if __name__ == '__main__':
+    fptr = tempfile.NamedTemporaryFile(mode='w')
+    
+    input = input.readline()
+
+    args = input
+    
+    result = function(args)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
