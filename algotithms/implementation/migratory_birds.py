bf14fdc872bacbfcaf4ce088d233cfdfc09c3b60
Hugo Souto <hugomsouto@gmail.com>
2023-10-30 05:15:52 -0300

    Initiate challenge

---
+++ algotithms/implementation/migratory_birds.py
@@ -0,0 +1,54 @@
+'''
+Challenge: Migratory Birds
+Difficulty: Easy
+Topic: Problem Solving (Basic)
+Max Score: 10
+Success Rate: 91.87%
+Task: Determine which type of bird in a flock occurs at the highest frequency.
+Problem: https://www.hackerrank.com/challenges/[...]/problem
+'''
+
+# Imports
+import math
+import os
+import random
+import re
+import sys
+import tempfile
+from io import StringIO
+
+# Simulate input from HackerRank
+raw_data = '''6
+1 4 4 4 5 3
+'''
+    # Expected Output:
+    # 4
+
+# raw_data = '''11
+# 1 2 3 4 5 4 3 2 1 3 4
+# '''
+#     # Expected Output:
+#     # 3
+
+# Data
+input = StringIO(raw_data)
+
+# Function
+def migratoryBirds(arr):
+    return result
+
+# Main
+if __name__ == '__main__':
+    fptr = tempfile.NamedTemporaryFile(mode='w')
+    
+    arr_count = int(input.readline().strip())
+
+    arr = list(map(int, input.readline().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+    print(result)
