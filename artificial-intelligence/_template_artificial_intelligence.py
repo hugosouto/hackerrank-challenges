fbe60318227bd5ff0fe84b10f5047b3ed0483f5e
Hugo Souto <hugomsouto@gmail.com>
2023-11-11 00:24:46 -0300

    Add fiile

---
+++ artificial-intelligence/_template_artificial_intelligence.py
@@ -0,0 +1,26 @@
+'''
+Challenge: 
+Difficulty: 
+Max Score: 
+Success Rate: 
+Task: 
+Problem: https://www.hackerrank.com/challenges/[...]/problem
+'''
+
+# Imports
+import tempfile
+from io import StringIO
+
+# Simulate input from HackerRank
+raw_data = '''
+'''
+    # Expected Output:
+    # 
+
+# Data
+input = StringIO(raw_data)
+
+# Function
+def function(args):
+    result = ''
+    return result
