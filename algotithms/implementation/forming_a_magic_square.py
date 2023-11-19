cdac9a2c0fc5069d7d06e0391a2b87cc9d870af1
Hugo Souto <hugomsouto@gmail.com>
2023-11-19 20:52:52 -0300

    First attempt with many errors

--- algotithms/implementation/forming_a_magic_square.py
+++ algotithms/implementation/forming_a_magic_square.py
@@ -13,26 +13,95 @@ import math
 import os
 import random
 import re
+from ssl import enum_certificates
 import sys
 import tempfile
 from io import StringIO
 
 # Simulate input from HackerRank
-raw_data = '''5 3 4
+raw_data = '''\
+5 3 4
 1 5 8
 6 4 2
 '''
     # Expected Output:
-    # 8 3 4
-    # 1 5 9
-    # 6 7 2
+    # 7
+
+# raw_data = '''\
+# 4 9 2
+# 3 5 7
+# 8 1 5
+# '''
+    # Expected Output:
+    # 1
+
+# raw_data = '''\
+# 4 8 2
+# 4 5 7
+# 6 1 6
+# '''
+    # Expected Output:
+    # 4
+
+# raw_data = '''\
+# 4 5 8
+# 2 4 1
+# 1 9 7
+# '''
+    # Expected Output:
+    # 14
 
 # Data
 input = StringIO(raw_data)
 
 # Function
 def formingMagicSquare(s):
-    return result
+    
+    original_matrix = s
+
+    magic_sum = sum(list(range(1,10)))
+    print('Magic Matrix Sum:', magic_sum)
+    matrix_sum = sum([sum(row) for row in s])
+    print('Matrix Sum:      ', matrix_sum)
+
+    cost = 0
+    
+    numbers_used = []
+    for row in s:
+        if sum(row) < 15: # 15 is the Magic Number
+            row_sum = sum(row)
+            sorted_row = sorted(row, reverse=True)
+            max_value = sorted_row[0]
+            if max_value in numbers_used:
+                max_value = sorted_row[1]
+                if max_value in numbers_used:
+                    max_value = sorted_row[2]
+            max_index = row.index(max_value)
+            row_cost = 15 - row_sum
+            row[max_index] += row_cost
+            cost += row_cost
+            numbers_used += row
+
+        if sum(row) > 15: # 15 is the Magic Number
+            row_sum = sum(row)
+            sorted_row = sorted(row, reverse=True)
+            max_value = sorted_row[0]
+            if max_value in numbers_used:
+                max_value = sorted_row[1]
+                if max_value in numbers_used:
+                    max_value = sorted_row[2]
+            max_index = row.index(max_value)
+            row_cost = row_sum - 15
+            row[max_index] -= row_cost
+            cost += row_cost
+            numbers_used += row
+
+    c = 0
+    for column in zip(*original_matrix):
+        print(f"Column {c}: {sum(column)}")
+        c += 1
+
+    return cost
 
 # Main
 if __name__ == '__main__':
