41c679ee09f80e2d4317885740ef76d282dc8b92
Hugo Souto <hugomsouto@gmail.com>
2023-11-27 08:00:44 -0300

    Advances in code

--- algotithms/implementation/picking_numbers.py
+++ algotithms/implementation/picking_numbers.py
@@ -21,20 +21,41 @@ from io import StringIO
 raw_data = '''6
 4 6 5 3 3 1
 '''
-    # Expected Output:
-    # 3
+# Expected Output:
+# 3
 
 raw_data = '''6
 1 2 2 3 1 2
 '''
-    # Expected Output:
-    # 5
+# Expected Output:
+# 5
 
 # Data
 input = StringIO(raw_data)
 
 # Function
 def pickingNumbers(a):
+    numbers = sorted(a)
+    print(numbers)
+
+    lenghts = []
+    sub = []
+
+    print(range(len(numbers)))
+
+    for slice_begin in range(0, len(numbers) - 1):
+        print('slice_begin', numbers[slice_begin])
+        # cursor = numbers[slice_begin + 1]
+        for slice_end in range(slice_begin + 1, len(numbers) - 1):
+            if slice_end - slice_begin <= 1:
+                slice_end += 1
+                print('slice_end', slice_end)
+            else:
+                lenghts.append(len(numbers[slice_begin:slice_end]))
+        # print(sub)
+    print('lenghts', lenghts)
+
+    return max(lenghts)
 
 # Main
 if __name__ == '__main__':
