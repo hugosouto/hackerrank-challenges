2246d967fad86a3924b7cfe1e20d84961255bcf7
Hugo Souto <hugomsouto@gmail.com>
2023-12-22 12:04:51 -0300

    Update file

--- functional-programming/Scala/src/main/scala/_TemplateScala.scala
+++ functional-programming/Scala/src/main/scala/_TemplateScala.scala
@@ -7,26 +7,3 @@ Success Rate:
 Task: 
 Problem: https://www.hackerrank.com/challenges/[...]/problem
 */
-
-// Imports
-// import java.io.StringReader
-// import java.util.Scanner
-
-// Simulate input from HackerRank
-// val raw_data = """
-// """
-// Expected Output:
-// 
-
-// Data
-// val input = new Scanner(new StringReader(raw_data))
-
-// Function
-// def function(args: Any): Any = {
-  // function body here
-// }
-
-// Main
-// def main(args: Array[String]): Unit = {
-  // val line = input.nextLine()
-// }
