73a3aaa832a876d7978534018f39edddc43792d5
Hugo Souto <hugomsouto@gmail.com>
2023-12-22 10:37:26 -0300

    Add file

---
+++ functional-programming/_template_scala.scala
@@ -0,0 +1,32 @@
+/*
+Challenge: 
+Difficulty: 
+Topic: 
+Max Score: 
+Success Rate: 
+Task: 
+Problem: https://www.hackerrank.com/challenges/[...]/problem
+*/
+
+// Imports
+import java.io.StringReader
+import java.util.Scanner
+
+// Simulate input from HackerRank
+val raw_data = """
+"""
+// Expected Output:
+// 
+
+// Data
+val input = new Scanner(new StringReader(raw_data))
+
+// Function
+def function(args: Any): Any = {
+  // function body here
+}
+
+// Main
+def main(args: Array[String]): Unit = {
+  val line = input.nextLine()
+}
