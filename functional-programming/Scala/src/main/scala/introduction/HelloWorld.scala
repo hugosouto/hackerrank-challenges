42d0e780a8844086486c33a3993fe7967262ed71
Hugo Souto <hugomsouto@gmail.com>
2023-12-22 12:05:18 -0300

    Initiate challenge

---
+++ functional-programming/Scala/src/main/scala/introduction/HelloWorld.scala
@@ -0,0 +1,16 @@
+/*
+Challenge: Hello World
+Difficulty: Easy
+Topic: Problem Solving (Basic)
+Max Score: 5
+Success Rate: 93.93%
+Task: Write code to print the 'Hello World' program.
+Link: https://www.hackerrank.com/challenges/fp-hello-world/problem
+*/
+
+package introduction
+
+// Sample Input:
+// (Nil)  
+
+def f() = println("")
