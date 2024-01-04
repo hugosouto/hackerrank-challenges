eb5952b0e44ffc4afe0d58e0c6c4de3ea80929b5
Hugo Souto <hugomsouto@gmail.com>
2024-01-03 21:58:08 -0300

    Initiate challenge

---
+++ python/collections/defaultdict_tutorial.py
@@ -0,0 +1,39 @@
+'''
+Challenge: DefaultDict Tutorial
+Difficulty: Easy
+Topic: Python (Basic)
+Max Score: 20
+Success Rate: 92.56%
+Task: Create dictionary value fields with predefined data types.
+Problem: https://www.hackerrank.com/challenges/defaultdict-tutorial/problem
+'''
+
+# Example
+from collections import defaultdict
+d = defaultdict(list)
+d['python'].append("awesome")
+d['something-else'].append("not relevant")
+d['python'].append("language")
+for i in d.items():
+    print(i)
+
+# # Imports
+# from io import StringIO
+
+# # Simulate input from HackerRank
+# raw_data = '''
+# '''
+#     # Expected Output:
+#     # 1 3
+#     # -1
+
+# # Data
+# input = StringIO(raw_data)
+
+# # Function
+# def function(args):
+#     return
+
+# # Main
+# if __name__ == '__main__':
+#     input = input.readline()
