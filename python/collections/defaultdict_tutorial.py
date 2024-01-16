d8e88e3cc1a47e0020fae4c7d53a44abfa7867be
Hugo Souto <hugomsouto@gmail.com>
2024-01-16 16:53:19 -0300

    Update code

--- python/collections/defaultdict_tutorial.py
+++ python/collections/defaultdict_tutorial.py
@@ -9,31 +9,31 @@ Problem: https://www.hackerrank.com/challenges/defaultdict-tutorial/problem
 '''
 
 # Example
-from collections import defaultdict
-d = defaultdict(list)
-d['python'].append("awesome")
-d['something-else'].append("not relevant")
-d['python'].append("language")
-for i in d.items():
-    print(i)
+# from collections import defaultdict
+# d = defaultdict(list)
+# d['python'].append("awesome")
+# d['something-else'].append("not relevant")
+# d['python'].append("language")
+# for i in d.items():
+#     print(i)
 
-# # Imports
-# from io import StringIO
+# Imports
+from io import StringIO
 
-# # Simulate input from HackerRank
-# raw_data = '''
-# '''
-#     # Expected Output:
-#     # 1 3
-#     # -1
+# Simulate input from HackerRank
+raw_data = '''
+'''
+    # Expected Output:
+    # 1 3
+    # -1
 
-# # Data
-# input = StringIO(raw_data)
+# Data
+input = StringIO(raw_data)
 
-# # Function
-# def function(args):
-#     return
+# Function
+def function(args):
+    return
 
-# # Main
-# if __name__ == '__main__':
-#     input = input.readline()
+# Main
+if __name__ == '__main__':
+    input = input.readline()
