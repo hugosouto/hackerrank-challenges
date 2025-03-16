1260525c4e49b958914fd29da9872e21b16b1302
Hugo Souto <hugomsouto@gmail.com>
2025-03-16 16:14:32 -0300

    Update Main section to implement solution, unfinished

--- python/collections/defaultdict_tutorial.py
+++ python/collections/defaultdict_tutorial.py
@@ -3,7 +3,7 @@ Challenge: DefaultDict Tutorial
 Difficulty: Easy
 Topic: Python (Basic)
 Max Score: 20
-Success Rate: 92.56%
+Success Rate: 92.99%
 Task: Create dictionary value fields with predefined data types.
 Problem: https://www.hackerrank.com/challenges/defaultdict-tutorial/problem
 '''
@@ -18,10 +18,18 @@ Problem: https://www.hackerrank.com/challenges/defaultdict-tutorial/problem
 #     print(i)
 
 # Imports
+from collections import defaultdict
 from io import StringIO
 
 # Simulate input from HackerRank
-raw_data = '''
+raw_data = '''5 2
+a
+a
+b
+a
+b
+a
+b
 '''
     # Expected Output:
     # 1 3
@@ -31,9 +39,24 @@ raw_data = '''
 input = StringIO(raw_data)
 
 # Function
-def function(args):
-    return
+d = defaultdict(list)
+def function(word, lst):
+    for i in A:
+        if word is in lst:
+            d['A'].append(A[i])
 
 # Main
 if __name__ == '__main__':
-    input = input.readline()
+    n, m = map(int, input.readline().split())
+    n_inputs = (n+m)
+
+    A = []
+
+    for i in range(n_inputs):
+        word = str(input.readline().strip())
+        if i < n:
+            A.append(word)
+
+        function(word, A)
+
+    print(d['A'])
